cc4eb5b43218fb5899dad2c7e58c5d0061bd1154
josue-org-pe <josue.monjemaron@gmail.com>
2026-06-01 18:35:00 -0500

    Guardando cambios antes del rebase

--- include/hash.h
+++ include/hash.h
@@ -3,14 +3,11 @@
 
 #include "arena.h"
 #include <stdbool.h>
-#include <stddef.h>
-#include <time.h>
 
 // Nodo de la lista enlazada para manejar colisiones de colisión hash
 typedef struct HashEntry {
     char* key;
     char* value;
-    time_t expire_at;
     struct HashEntry* next;
 } HashEntry;
 
@@ -24,9 +21,5 @@ typedef struct {
 HashTable* hash_create(Arena* arena, size_t size);
 bool hash_put(HashTable* ht, const char* key, const char* value);
 const char* hash_get(HashTable* ht, const char* key);
-bool hash_delete(HashTable* ht, const char* key);
-bool hash_exists(HashTable* ht, const char* key);
-bool hash_set_expire(HashTable* ht, const char* key, int seconds);
-int hash_ttl(HashTable* ht, const char* key);
 
 #endif
--- include/wal.h
+++ include/wal.h
@@ -14,8 +14,6 @@ Wal* wal_init(const char* filepath);
 
 // Registra la acción "PUT" en disco antes de que explote la máquina
 bool wal_append_put(Wal* wal, const char* key, const char* value);
-bool wal_append_del(Wal* wal, const char* key);
-bool wal_append_expire(Wal* wal, const char* key, int seconds);
 
 // Cierra el archivo de forma segura
 void wal_close(Wal* wal);
